a6977dfa0ccfb85318d72e74f09a096fbdbc7680
Rennan Cockles <r3ck.dev@gmail.com>
2025-04-14 08:52:09 -0300

    some changes required to make it work with bruce firmware

--- src/MFRC522DriverI2C.cpp
+++ src/MFRC522DriverI2C.cpp
@@ -7,17 +7,22 @@
 
 bool MFRC522DriverI2C::init() {
   // TODO avoid double init.
-  _wire.begin(); // Returns type void, no check possible.
+  if (_sdaPin != -1 && _sclPin != -1) {
+    _wire.begin(_sdaPin, _sclPin);
+  } else {
+    _wire.begin();
+  }
   return true;
 }
 
 /**
  * Writes a byte to the specified register in the MFRC522 chip.
- * The interface is described in the datasheet section 8.1.4. 
+ * The interface is described in the datasheet section 8.1.4.
  */
-void MFRC522DriverI2C::PCD_WriteRegister(const PCD_Register reg,    ///< The register to write to. One of the PCD_Register enums.
-                                         const byte value           ///< The value to write.
-                                        ) {
+void MFRC522DriverI2C::PCD_WriteRegister(
+    const PCD_Register reg, ///< The register to write to. One of the PCD_Register enums.
+    const byte value        ///< The value to write.
+) {
   _wire.beginTransmission(_slaveAdr);
   _wire.write(reg);
   _wire.write(value);
@@ -28,10 +33,11 @@ void MFRC522DriverI2C::PCD_WriteRegister(const PCD_Register reg,    ///< The reg
  * Writes a number of bytes to the specified register in the MFRC522 chip.
  * The interface is described in the datasheet section 8.1.4.
  */
-void MFRC522DriverI2C::PCD_WriteRegister(const MFRC522Constants::PCD_Register reg,    ///< The register to write to. One of the PCD_Register enums.
-                                         const byte count,        ///< The number of bytes to write to the register.
-                                         byte *const values        ///< The values to write. Byte array.
-                                        ) {
+void MFRC522DriverI2C::PCD_WriteRegister(
+    const MFRC522Constants::PCD_Register reg, ///< The register to write to. One of the PCD_Register enums.
+    const byte count,                         ///< The number of bytes to write to the register.
+    byte *const values                        ///< The values to write. Byte array.
+) {
   _wire.beginTransmission(_slaveAdr);
   _wire.write(reg);
   _wire.write(values, count);
@@ -42,19 +48,21 @@ void MFRC522DriverI2C::PCD_WriteRegister(const MFRC522Constants::PCD_Register re
  * Reads a byte from the specified register in the MFRC522 chip.
  * The interface is described in the datasheet section 8.1.4.
  */
-byte MFRC522DriverI2C::PCD_ReadRegister(const PCD_Register reg    ///< The register to read from. One of the PCD_Register enums.
-                                       ) {
+byte MFRC522DriverI2C::PCD_ReadRegister(
+    const PCD_Register reg ///< The register to read from. One of the PCD_Register enums.
+) {
   byte value;
-  
+
   _wire.beginTransmission(_slaveAdr);
   _wire.write(reg);
   _wire.endTransmission();
-  
+
   _wire.requestFrom(_slaveAdr, (uint8_t)1);
-  //while(!_wire.available()); // Dangerous! Might block! Wait for byte to be available. TODO timeout detection.
+  // while(!_wire.available()); // Dangerous! Might block! Wait for byte to be available. TODO timeout
+  // detection.
   value = (uint8_t)_wire.read();
   _wire.endTransmission();
-  
+
   return value;
 } // End PCD_ReadRegister()
 
@@ -62,44 +70,41 @@ byte MFRC522DriverI2C::PCD_ReadRegister(const PCD_Register reg    ///< The regis
  * Reads a number of bytes from the specified register in the MFRC522 chip.
  * The interface is described in the datasheet section 8.1.2.
  */
-void MFRC522DriverI2C::PCD_ReadRegister(const PCD_Register reg,    ///< The register to read from. One of the PCD_Register enums.
-                                        const byte count,          ///< The number of bytes to read.
-                                        byte *const values,        ///< Byte array to store the values in.
-                                        const byte rxAlign         ///< Only bit positions rxAlign..7 in values[0] are updated.
-                                       ) {
+void MFRC522DriverI2C::PCD_ReadRegister(
+    const PCD_Register reg, ///< The register to read from. One of the PCD_Register enums.
+    const byte count,       ///< The number of bytes to read.
+    byte *const values,     ///< Byte array to store the values in.
+    const byte rxAlign      ///< Only bit positions rxAlign..7 in values[0] are updated.
+) {
   // Sanity check.
-  if(count == 0 || values == nullptr) {
-    return;
-  }
-  
+  if (count == 0 || values == nullptr) { return; }
+
   byte index = 0;
-  
+
   _wire.beginTransmission(_slaveAdr);
-  _wire.write(reg);  // Tell MFRC522 which register address we want to read.
+  _wire.write(reg); // Tell MFRC522 which register address we want to read.
   _wire.endTransmission();
-  
+
   _wire.requestFrom(_slaveAdr, count);
-  
+
   // Todo: is waiting for byte to be available required?
   // With thanks to arozcan (https://github.com/arozcan/MFRC522-I2C-Library), but slightly modified:
-  while(_wire.available() && index < count) {
-    if(index == 0 && rxAlign) {    // Only update bit positions rxAlign..7 in values[0]
+  while (_wire.available() && index < count) {
+    if (index == 0 && rxAlign) { // Only update bit positions rxAlign..7 in values[0]
       // Create bit mask for bit positions rxAlign..7
       byte mask = 0;
-      
-      for(byte i     = rxAlign; i <= 7; i++) {
-        mask |= (1 << i);
-      }
+
+      for (byte i = rxAlign; i <= 7; i++) { mask |= (1 << i); }
       // Read value and tell that we want to read the same address again.
-      byte     value = (byte)_wire.read(); // returns int but only with uint8 content
-      
+      byte value = (byte)_wire.read(); // returns int but only with uint8 content
+
       // Apply mask to both current value of values[0] and the new data in value.
       values[0] = (values[index] & ~mask) | (value & mask);
-    } else { // Normal case
+    } else {                              // Normal case
       values[index] = (byte)_wire.read(); // returns int but only with uint8 content
     }
     index++;
   }
-  
+
   // Fixme: flush wire if still data available?
 } // End PCD_ReadRegister()
--- src/MFRC522DriverSPI.h
+++ src/MFRC522DriverSPI.h
@@ -1,43 +1,55 @@
 /* SPDX-License-Identifier: LGPL-2.1 */
 #pragma once
 
-#include <SPI.h>
 #include <MFRC522Driver.h>
 #include <MFRC522DriverPin.h>
+#include <SPI.h>
 
 class MFRC522DriverSPI : public MFRC522Driver {
 public:
-  //using PCD_Register = MFRC522Constants::PCD_Register;
+  // using PCD_Register = MFRC522Constants::PCD_Register;
   /////////////////////////////////////////////////////////////////////////////////////
   // Functions for setting up the Arduino.
   /////////////////////////////////////////////////////////////////////////////////////
-  
+
   bool init() override;
-  
+
   /////////////////////////////////////////////////////////////////////////////////////
   // Basic interface functions for communicating with the MFRC522.
   /////////////////////////////////////////////////////////////////////////////////////
   void PCD_WriteRegister(const PCD_Register reg, const byte value) override;
   void PCD_WriteRegister(const PCD_Register reg, const byte count, byte *const values) override;
   byte PCD_ReadRegister(const PCD_Register reg) override;
-  void PCD_ReadRegister(const PCD_Register reg, const byte count, byte *const values, const byte rxAlign = 0) override;
-  
-  
+  void PCD_ReadRegister(
+      const PCD_Register reg, const byte count, byte *const values, const byte rxAlign = 0
+  ) override;
+
   // Advanced init with custom chip select method, e.g. with i2c port expander or multiplexer.
-  MFRC522DriverSPI(MFRC522DriverPin &chipSelectPin,
-                   SPIClass &spiClass = SPI,
-                   const SPISettings spiSettings = SPISettings(4000000u /* 4MHz */, MSBFIRST, SPI_MODE0)
-                  ) : MFRC522Driver(),
-                      _chipSelectPin(chipSelectPin),
-                      _spiClass(spiClass),
-                      _spiSettings(spiSettings) {};
+  MFRC522DriverSPI(
+      MFRC522DriverPin &chipSelectPin, SPIClass &spiClass = SPI,
+      const SPISettings spiSettings = SPISettings(4000000u /* 4MHz */, MSBFIRST, SPI_MODE0)
+  )
+      : MFRC522Driver(), _chipSelectPin(chipSelectPin), _spiClass(spiClass), _spiSettings(spiSettings) {};
+
+  MFRC522DriverSPI(
+      MFRC522DriverPin &chipSelectPin, int8_t sck = -1, int8_t miso = -1, int8_t mosi = -1,
+      SPIClass &spiClass = SPI,
+      const SPISettings spiSettings = SPISettings(4000000u /* 4MHz */, MSBFIRST, SPI_MODE0)
+  )
+      : MFRC522Driver(), _chipSelectPin(chipSelectPin), _sck(sck), _miso(miso), _mosi(mosi),
+        _spiClass(spiClass), _spiSettings(spiSettings) {};
 
 protected:
   // Pins.
-  //byte _chipSelectPin;        // Arduino pin connected to MFRC522's SPI slave select input (Pin 24, NSS, active low)
-  MFRC522DriverPin &_chipSelectPin; // Arduino pin connected to MFRC522's SPI slave select input (Pin 24, NSS, active low)
-  
+  // byte _chipSelectPin;        // Arduino pin connected to MFRC522's SPI slave select input (Pin 24, NSS,
+  // active low)
+  MFRC522DriverPin
+      &_chipSelectPin; // Arduino pin connected to MFRC522's SPI slave select input (Pin 24, NSS, active low)
+
   // SPI communication.
-  SPIClass &_spiClass;        // SPI class which abstracts hardware.
-  const SPISettings _spiSettings;    // SPI settings.
+  int8_t _sck;
+  int8_t _miso;
+  int8_t _mosi;
+  SPIClass &_spiClass;            // SPI class which abstracts hardware.
+  const SPISettings _spiSettings; // SPI settings.
 };
